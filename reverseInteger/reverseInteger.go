cef5a2762b895977e27db8111ff16eb121875c2b
Lau Jing Xuan <melvinlaujx@gmail.com>
2022-10-10 21:46:50 +0800

    reverseInteger

--- reverseInteger/reverseInteger.go
+++ reverseInteger/reverseInteger.go
@@ -1,20 +1,32 @@
 package main
 
-//Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+import (
+	"fmt"
+	"math"
+)
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 func reverse(x int) int {
-	num := x
 	reverseNum := 0
 	loopCount := 9 //As reverse of value >10 digits might cause overflow
-	for num > 0 && loopCount > 0 {
-		lastDigit := num % 10
+	for x != 0 && loopCount > 0 {
+		lastDigit := x % 10
 		reverseNum = reverseNum*10 + lastDigit
-		num = num / 10
-		loopCount = -1
+		x = x / 10
+		loopCount -= 1
 	}
-	if num == 0 {
+	if x == 0 {
 		return reverseNum
 	}
-	if x < 0 {
-
+	if reverseNum < math.MinInt32/10 || reverseNum > math.MaxInt32/10 {
+		return 0
 	}
+	if reverseNum == math.MinInt32/10 && (x < -8 || x > 7) {
+		return 0
+	}
+	return reverseNum*10 + x
+}
+
+func main() {
+	fmt.Println(reverse(-2147483412))
 }
