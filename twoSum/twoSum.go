60afc5da52b12295e678d59a2cafcd2633b35679
Lau Jing Xuan <melvinlaujx@gmail.com>
2022-10-14 16:25:03 +0800

    improve twoSums

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 func twoSum(nums []int, target int) []int {
 	for i, num := range nums {
 		val := target - num
@@ -16,6 +19,28 @@ func twoSum(nums []int, target int) []int {
 	return []int{}
 }
 
+func twoSumAlt(nums []int, target int) []int {
+	if len(nums) == 0 || len(nums) == 1 {
+		return []int{}
+	}
+	sort.Ints(nums)
+	start := 0
+	end := len(nums) - 1
+	for end > start {
+		temp := nums[start] + nums[end]
+		if temp == target {
+			return []int{start, end}
+		}
+		if temp > target {
+			end--
+			continue
+		}
+		start++
+	}
+	return []int{}
+}
+
 func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSumAlt([]int{11, 7, 2, 15}, 9))
 }
