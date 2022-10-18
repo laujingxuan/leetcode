634ba273013aa757e2d76d6a691396ca1b89cd1b
Lau Jing Xuan <melvinlaujx@gmail.com>
2022-10-18 22:39:44 +0800

    next larger and next smaller numbers

--- bit-manipulation-practice/Main.java
+++ bit-manipulation-practice/Main.java
@@ -74,14 +74,77 @@ public class Main {
   }
 
   //given a positive integer, print the next smallest and the next largest number that have the same number of 1 bits in their binary representation
-  static int[] nextNumber(int num){
+  static int[] nextNumber(int num) {
+    int[] toReturn = new int[2];
+    //identify the c0 and c1
+    int nextLarger = getNextLargerNumber(num);
+    int nextSmaller = getNextSmallerNumber(num);
+    toReturn[0] = nextLarger;
+    toReturn[1] = nextSmaller;
+    return toReturn;
+  }
 
+  static int getNextLargerNumber(int num) {
+    int c = num;
+    int c0 = 0;
+    int c1 = 0;
+
+    while (((c & 1) == 0) && (c != 0)) {
+      c0++;
+      c = c >> 1;
+    }
+
+    while ((c & 1) == 1) {
+      c1++;
+      c = c >> 1;
+    }
+
+    if (c0 + c1 == 31 || c0 + c1 == 0) {
+      return -1;
+    }
+
+    int p = c0 + c1;
+    //convert the p position 0 to be 1
+    num |= (1 << p);
+    //convert all the bits behind position p to be 0
+    num &= ~((1 << p) - 1);
+    //add back the number of 1 removed from the start
+    num |= (1 << c1 - 1) - 1;
+    return num;
+  }
+
+  static int getNextSmallerNumber(int num) {
+    int n = num;
+    int c0 = 0;
+    int c1 = 0;
+    while ((n & 1) == 1) {
+      c1++;
+      n = n >> 1;
+    }
+    if (n == 0) {
+      return -1;
+    }
+    while (((n & 1) == 0) && n != 0) {
+      c0++;
+      n = n >> 1;
+    }
+    int p = c0 + c1;
+    //convert p position 1 into 0
+    num ^= (1 << p);
+    //covert all numbers at the right of p into 0
+    num &= ~((1 << p) - 1);
+    //convert number immediate after p into 1 based on c1+1 count
+    num |= ((1 << (c1 + 1)) - 1) << (c0 - 1);
+    return num;
   }
 
   public static void main(String[] args) {
     // String binaryString = Integer.toBinaryString(updateBits(1024,19,2,6));
     // String binaryString = binaryToString(0.75);
     // System.out.println(binaryString);
-    System.out.println(flipBitToWin(1775));
+    // System.out.println(flipBitToWin(1775));
+    int[] answer = nextNumber(359);
+    System.out.println(answer[0]);
+    System.out.println(answer[1]);
   }
 }
