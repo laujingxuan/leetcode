4bbc609ff290d46c0d69ea87fd0b815803052836
Jing Xuan Lau <jingxuan.lau@shopee.com>
2022-10-19 19:01:12 +0800

    check symmetry

--- tree-graph-practice/main.go
+++ tree-graph-practice/main.go
@@ -226,4 +226,33 @@ func orderProjects(projects []*Project) []*Project {
 	return order
 }
 
+//----------------------------------------------------------------------is a binary node symmetry
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return isSame(root.Left, root.Right)
+}
+
+func isSame(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	//right of leftNode compare with left of rightNode recursively
+	return isSame(left.Right, right.Left) && isSame(left.Left, right.Right)
+}
+
+//----------------------------------------------------------------------is a binary node symmetry
+
 //bidrectional search
