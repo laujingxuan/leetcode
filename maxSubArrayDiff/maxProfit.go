58b3ad368ef4f54d4a489c221716e93767e3b1e4
Jing Xuan Lau <jingxuan.lau@shopee.com>
2022-11-07 15:44:29 +0800

    max sub array diff

--- maxSubArrayDiff/maxProfit.go
+++ maxSubArrayDiff/maxProfit.go
@@ -35,3 +35,31 @@ func maxProfit(prices []int) int {
 	}
 	return 0
 }
+
+func maxProfitBetter(prices []int) int {
+	min := math.MaxInt
+	currentMaxDifference := 0
+	for i := 0; i < len(prices)-1; i++ {
+		if prices[i] < min {
+			min = prices[i]
+		}
+		currentDifference := prices[i+1] - min
+		if currentDifference > currentMaxDifference {
+			currentMaxDifference = currentDifference
+		}
+	}
+	if min != math.MaxInt {
+		return currentMaxDifference
+	}
+	return 0
+}
+
+func maxProfitAlternate(prices []int) int {
+	maxCurr := 0
+	maxSoFar := 0
+	for i := 0; i < len(prices)-1; i++ {
+		maxCurr = int(math.Max(0, float64(maxCurr+prices[i+1]-prices[i])))
+		maxSoFar = int(math.Max(float64(maxCurr), float64(maxSoFar)))
+	}
+	return maxSoFar
+}
