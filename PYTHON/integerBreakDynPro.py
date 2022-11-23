611c5ad672c1afa2e36734daab64dda1db48cf13
Lau Jing Xuan <melvinlaujx@gmail.com>
2022-11-23 21:34:10 +0800

    integer break with math solution

--- PYTHON/integerBreakDynPro.py
+++ PYTHON/integerBreakDynPro.py
@@ -1,31 +1,36 @@
 #Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
 #Return the maximum product you can get.
 def integerBreak(n):
-    memo = [0] * (n+1)
-    return integerBreakHelper(n, memo, 1, 0)
+    return integerBreakHelper(n, 1, 0)
 
-def integerBreakHelper(n, memo, currentMultiplication, count):
+def integerBreakHelper(n, currentMultiplication, count):
     if (n == 1 or n == 0) and count >= 2:
         return currentMultiplication
     
     if n==0:
         return 0
 
-    # if memo[n] != 0:
-    #     return memo[n]
-
     largestMultiplication = 0
     for i in range(1, n+1):
-        # if i == 2 and n == 4:
-        #     print("i:" + str(i))
-        #     print(currentMultiplication)
-        #     print(count)
-        #     print("n-i: " + str(n-i))
-        output = integerBreakHelper(n-i, memo, currentMultiplication * i, count+1)
+        output = integerBreakHelper(n-i, currentMultiplication * i, count+1)
         largestMultiplication = max(largestMultiplication, output)
-    memo[n] = largestMultiplication
-    # print(memo)
-    return memo[n]
+    return largestMultiplication
+
+
+def integerBreakMath(n):
+    if n == 2:
+        return 1
+    if n == 3:
+        return 2
+    remainder = n%3
+    numberOfThree = n//3
+    if remainder == 0:
+        return 3 ** numberOfThree
+    if remainder == 1:
+        return (3 ** (numberOfThree-1)) * 4
+    else:
+        return (3 ** numberOfThree) * 2
 
 if __name__ == '__main__':
-    print(integerBreak(10))
+    print(integerBreak(24))
+    print(integerBreakMath(24))
