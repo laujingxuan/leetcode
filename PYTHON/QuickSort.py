9a4a7b42c02feb4c041d7515fb8bb13f0ac8ba4c
Jing Xuan Lau <jingxuan.lau@shopee.com>
2023-01-04 17:24:40 +0800

    quick sort practice

--- PYTHON/QuickSort.py
+++ PYTHON/QuickSort.py
@@ -1,4 +1,7 @@
 # https://www.geeksforgeeks.org/quick-sort/
+import random
+
+
 def partition(arr, low, high):
     i = low
     pivot = arr[high]
@@ -16,5 +19,28 @@ def quickSort(arr, low, high):
         quickSort(arr, low, pivot - 1)
     return arr
 
+def partitionAlt(array, low, high):
+    #to randomized the number
+    randomNumber = random.randrange(low, high+1)
+    array[randomNumber], array[high] = array[high], array[randomNumber]
+
+    anchor = array[high]
+    smallerIndex = low
+    for i in range(low, high):
+        if array[i] <= anchor:
+            array[smallerIndex], array[i] = array[i], array[smallerIndex]
+            smallerIndex += 1
+    array[smallerIndex], array[high] = array[high], array[smallerIndex]
+    return smallerIndex
+
+
+def randomizedQuickSort(array, low, high):
+    if high > low:
+        index = partitionAlt(array, low, high)
+        randomizedQuickSort(array, low, index - 1)
+        randomizedQuickSort(array, index + 1, high)
+    return array
+
 if __name__ == "__main__":
     print(quickSort([7,4,6,1,3], 0, 4))
+    print(randomizedQuickSort([7,4,6,1,3], 0, 4))
