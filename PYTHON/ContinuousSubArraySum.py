a9f47a3bf3742a4c2a521aaf0f0c7f74d2cce1e2
Jing Xuan Lau <jingxuan.lau@shopee.com>
2023-01-13 11:04:14 +0800

    continuous subarray sum

---
+++ PYTHON/ContinuousSubArraySum.py
@@ -0,0 +1,26 @@
+class Solution:
+    #time limit exceeded
+    def checkSubarraySumBruteForce(self, nums, k):
+        for i in range(len(nums)):
+            tempSum = nums[i]
+            for j in range(i+1, len(nums)):
+                tempSum += nums[j]
+                if tempSum%k == 0:
+                    return True
+        return False
+
+    def checkSubarraySum(self, nums, k):
+        #Basically is if we find a duplicate remainder in the accumulated sum array, it means that there's a part of subarray with sum%k==0
+        #Two remainders the same means the sum of difference in between the two indexes are a multiple of k
+        checkRemainder = {}
+        checkRemainder[0] = -1
+        currentSum = 0
+        for i in range(len(nums)):
+            currentSum += nums[i]
+            remainder = currentSum%k
+            if remainder in checkRemainder:
+                if i - checkRemainder[remainder] > 1:
+                    return True
+            else:
+                checkRemainder[remainder] = i
+        return False
