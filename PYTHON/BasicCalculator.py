2cb2a239f43d378fc191fc07c7fa435267bcba60
Jing Xuan Lau <jingxuan.lau@shopee.com>
2023-02-08 15:16:14 +0800

    calculator

--- PYTHON/BasicCalculator.py
+++ PYTHON/BasicCalculator.py
@@ -35,6 +35,44 @@ class Solution:
         total += num
         return total
 
+    #slightly worse time complexity as above ideal solution is just one pass
+    def calculateNonIdeal(self, s: str) -> int:
+        total = 0
+        sign = "+"
+        num = 0
+        index = 0
+        while index < len(s):
+            if s[index].isdigit():
+                num = num*10 + int(s[index])
+            if not s[index].isdigit() and s[index] != " ":
+                if s[index] == "(":
+                    start = index + 1
+                    noOfOpen = 1
+                    index += 1
+                    while noOfOpen > 0:
+                        if s[index] == ")":
+                            noOfOpen -=1
+                        if s[index] == "(":
+                            noOfOpen += 1
+                        # print(noOfOpen)
+                        index += 1
+                    # print(s[start:index - 1])
+                    num = self.calculate(s[start:index - 1])
+                    # print(num)
+                    continue
+                if sign == "+":
+                    total += num
+                if sign == "-":
+                    total -= num
+                num = 0
+                sign = s[index]
+            index += 1
+        if sign == "-":
+            total -= num
+        else:
+            total += num
+        return total
+
 if __name__ == "__main__":
     test = Solution()
     print(test.calculate("(1+(4+5+2)-3)+(6+8)"))
