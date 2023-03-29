7c19cb7a7d9c5ad1ed987617acbf1b96d11d0e4f
Jing Xuan Lau <jingxuan.lau@shopee.com>
2023-03-29 10:47:47 +0800

    update min cost of tickets

--- PYTHON/minCostOfTickets.py
+++ PYTHON/minCostOfTickets.py
@@ -10,7 +10,34 @@
 # For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
 # Return the minimum number of dollars you need to travel every day in the given list of days.
 
+import math
+
 class Solution:
+
+    def mincostTickets(self, days, costs):
+        minArrays = [-1 for i in range(len(days))]
+        nextAvailableDayIndex = {}
+
+        currentAvailableDayIndex = 0
+        for i in range(days[-1] + 1):
+            if i > days[currentAvailableDayIndex]:
+                currentAvailableDayIndex += 1
+            nextAvailableDayIndex[i] = currentAvailableDayIndex
+        costMap = {costs[0]:1, costs[1]:7, costs[2]:30}
+
+        for dayIndex in range(len(days) - 1, -1, -1):
+            minCostNeeded = math.inf
+            for cost in costs:
+                newDay = costMap[cost] + days[dayIndex]
+                if newDay > days[-1]:
+                    minCostNeeded = min(minCostNeeded, cost)
+                else:
+                    minCostNeeded = min(minCostNeeded, cost + minArrays[nextAvailableDayIndex[newDay]])        
+            minArrays[dayIndex] = minCostNeeded
+        return minArrays[0]
+
+
+
     def mincostTicketsTimeLimitExceeded(self, days, costs):
         costsMap = {costs[0]:1, costs[1]:7, costs[2]:30}
         return self.backTrackHelper(days, costsMap, 0, 0)
